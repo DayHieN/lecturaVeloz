ede9196a9f4708e5301f5e2dd96356a572ecdb1b
HieN <sanblas.cerumen@gmail.com>
2023-06-29 04:26:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# speedRead
+
+Herramienta que muestra un texto por pantalla, una palabra a la vez, dependiendo de las palabras por minuto (wpm) especificadas.
+Para facilitar la lectura y leer rápido.
+
+## Uso
+
+``python speedRead <textfile> <wpm>``
+
+### ToDo
+
+- [X] Funcionalidad básica
+- [ ] Optical Recognition Point (Punto de reconocimiento Óptico)
---
+++ lecturaVeloz.py
@@ -0,0 +1,27 @@
+from time import sleep
+from termcolor import colored
+import sys
+
+
+
+def openTxt(txt):
+    with open(txt, 'r', encoding="utf-8") as t:
+        text = t.read().split()
+        return text
+
+
+def displayWords(txt, wpm):
+
+    space = ' '
+    delay = round((60 / wpm), 2)
+
+    for word in txt:
+        print(f'{space*20}{colored(word, "green")}', end='\r')
+        print(end='\x1b[2K')
+
+        sleep(delay)
+
+
+txt = openTxt(sys.argv[1])
+
+displayWords(txt, int(sys.argv[2]))
